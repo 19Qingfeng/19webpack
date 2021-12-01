9a2e1ee85f7863909d995154b5dae2c31256bac6
wang.haoyu <228233464@qq.com>
2021-12-01 21:53:55 +0800

    feat(loader): url-loader

---
+++ loader-runner/loaders/url-loader/index.js
@@ -0,0 +1,28 @@
+const mime = require('mime');
+
+/**
+ * urlLoader是基于fileLoader进行实现的
+ * 当文件大小超过limit限制 使用file-loader处理
+ * 当文件大小小于limit限制 使用url-loader将文件转化为base64格式内嵌
+ */
+function urlLoader(source) {
+  const options = this.getOptions() || {};
+  const limit = options.limit || 1024;
+  const fallback = options.fallback;
+  if (limit && source.length < limit) {
+    // 获取文件类型
+    const fileType = mime.getType(this.resourcePath);
+    // 拼接base64
+    const base64str = `data:${fileType};base64,${source.toString('base64')}`;
+    return `module.exports = ${JSON.stringify(base64str)}`;
+  } else {
+    // 没有设置limit 或者limit < source.length 资源超出大小
+    // 拿到备用loader函数
+    const fallbackLoader = require(fallback);
+    return fallbackLoader.call(this, source);
+  }
+}
+
+urlLoader.raw = true;
+
+module.exports = urlLoader;
